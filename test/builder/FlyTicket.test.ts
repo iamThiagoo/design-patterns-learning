9fb62255a0132e6af2e931adbf7801c06afdb70f
Thiago Ferreira <thiagofer2003@outlook.com>
2025-01-21 23:33:32 -0300

    feat: builder

---
+++ test/builder/FlyTicket.test.ts
@@ -0,0 +1,18 @@
+import FlightTicket from "../../src/builder/FlightTicket";
+import FlightTicketBuilder from "../../src/builder/FlightTicketBuilder";
+
+test("Deve criar uma passagem aérea", () => {
+    const builder = new FlightTicketBuilder()
+        .setFlight("Azul", "1234")
+        .setTrip("CXS", "GRU")
+        .setPassenger("John Doe", "john.doe@gmail.com", "111.111.111-11", "M")
+        .setEmergencyContact("Jane Doe", "5554999999999")
+        .setSeat("1A")
+        .setCheckedBags(2)
+        .setCheckinInformation(true, "1", "4A")
+        .setPriority(5);
+
+    const flightTicket = new FlightTicket(builder);
+    expect(flightTicket.flightCode).toBe("1234");
+    expect(flightTicket.passengerName).toBe("John Doe");
+})
