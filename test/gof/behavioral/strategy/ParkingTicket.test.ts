aa646695628ecad056d384ff8016a58cd2d0d076
Thiago Ferreira <thiagofer2003@outlook.com>
2025-02-01 21:32:35 -0300

    feat: strategy

---
+++ test/gof/behavioral/strategy/ParkingTicket.test.ts
@@ -0,0 +1,7 @@
+import ParkingTicket from "../../../../src/gof/behavioral/strategy/ParkingTicket";
+
+test("Deve calcular a tarifa do veículo estacionado no aeroporto", async () => {
+    const parkingTicket = new ParkingTicket("AAA9999", new Date("2025-01-01T10:00:00"), "airport");
+    parkingTicket.checkout(new Date("2025-01-01T12:00:00"))
+    expect(parkingTicket.fare).toBe(20)
+})
